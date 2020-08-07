62edbe642d8652fbc417c4bf1a73f07e7cc4a9ae
Key <keyla@moogah.com>
2020-08-07 13:19:01 -0300

    fl_mrp_order_print added

---
+++ cliente/fl_mrp_order_print/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MRP Order Print",
+    'summary': """MRP Order Print""",
+    'description': """MRP Order Print""",
+    'author': "Moogah",
+    'website': "http://www.Moogah.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.0',
+    'depends': [
+        'mrp',
+        'report',
+    ],
+    'data': [
+        'views/view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'application': True,
+}
